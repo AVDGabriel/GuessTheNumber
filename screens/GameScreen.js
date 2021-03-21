da3f9110295ecddae1f9a6f86d54a3c0b97c1c17
AVDGAbriel <gabriel.avadani97@gmail.com>
2021-03-21 15:35:37 +0200

    using Dimensions in if checks

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -4,7 +4,8 @@ import {
     Text,
     StyleSheet,
     Alert,
-    FlatList
+    FlatList,
+    Dimensions
 } from 'react-native';
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
@@ -71,6 +72,11 @@ const GameScreen = props => {
         setPastGuesses(curPastGuesses => [nextNumber.toString(), ...curPastGuesses]);
     };
 
+    let listContainerStyle = styles.listContainer;
+    if (Dimensions.get('window').width < 500) {
+        listContainerStyle = styles.listContainerBig;
+    }
+
     return (
         <View style={styles.screen}>
             <Text style={DefaultStyles.title}>
@@ -87,7 +93,7 @@ const GameScreen = props => {
                     <Ionicons name='md-add' size={24} color='white' />
                 </MainButton>
             </Card>
-            <View style={styles.listContainer}>
+            <View style={listContainerStyle}>
                 {/* Now, in the bind function we can bind every parameter we want as the first parameter. 
                 Then the react will add the parameters that should be added by default, like the ItemData 
                 parameter which hold the item and the index attributes.
@@ -111,7 +117,7 @@ const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
-        marginTop: 20,
+        marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
         width: 400,
         maxWidth: '90%'
     },
@@ -127,6 +133,10 @@ const styles = StyleSheet.create({
     },
     listContainer: {
         width: '60%',
+        flex: 1
+    },
+    listContainerBig: {
+        width: '80%',
         flex: 1
     },
     list: {
