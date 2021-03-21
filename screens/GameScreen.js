1b6af46c14ca3030caad7e67c92e654c6f248d9c
AVDGAbriel <gabriel.avadani97@gmail.com>
2021-03-21 14:03:24 +0200

    styling list items and lists

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -4,6 +4,7 @@ import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
 import DefaultStyles from '../constants/default-styles';
 import MainButton from '../components/MainButton';
+import BodyText from '../components/BodyText';
 import { Ionicons } from '@expo/vector-icons';
 import { ScrollView } from 'react-native';
 
@@ -17,6 +18,13 @@ const generateRandomBetween = (min, max, exclude) => {
         return randomNumber;
     }
 };
+
+const renderListItem = (value, numOfRound) => (<View key={value} style={styles.listItem}>
+    <BodyText>#{numOfRound}</BodyText>
+    <BodyText>
+        {value}
+    </BodyText>
+</View>);
 
 const GameScreen = props => {
     //Object destructuring.
@@ -73,9 +81,11 @@ const GameScreen = props => {
                     <Ionicons name='md-add' size={24} color='white' />
                 </MainButton>
             </Card>
-            <ScrollView>
-                {pastGuesses.map(guess => <View key={guess}><Text>{guess}</Text></View>)}
-            </ScrollView>
+            <View style={styles.list}>
+                <ScrollView>
+                    {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
+                </ScrollView>
+            </View>
         </View>
     );
 };
@@ -92,6 +102,19 @@ const styles = StyleSheet.create({
         marginTop: 20,
         width: 400,
         maxWidth: '90%'
+    },
+    listItem: {
+        borderColor: '#ccc',
+        padding: 15,
+        marginVertical: 10,
+        backgroundColor: 'white',
+        borderWidth: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    list: {
+        width: '80%',
+        flex: 1
     }
 });
 
